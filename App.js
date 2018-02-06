9d5b5085c508196df3723480d0f3dae56eeb49b1
Simon Hoyos <shmesa23@gmail.com>
2018-02-06 14:31:56 -0500

    Show all examples

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Alert, Button, StyleSheet, Text, ScrollView, View, TouchableHighlight, TouchableOpacity, Picker, TextInput, Slider, Switch } from 'react-native';
+// import ExampleOne from './components/ExampleOne';
+// import ExampleTwo from './components/ExampleTwo';
+// import ExampleThree from './components/ExampleThree';
+// import ExampleFour from './components/ExampleFour';
+// import ExampleFive from './components/ExampleFive';
+// import ExampleSix from './components/ExampleSix';
+// import ExampleSeven from './components/ExampleSeven';
+
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        {/* <ExampleOne /> */}
+        {/* <ExampleTwo /> */}
+        {/* <ExampleThree /> */}
+        {/* <ExampleFour /> */}
+        {/* <ExampleFive /> */}
+        {/* <ExampleSix /> */}
+        {/* <ExampleSeven /> */}
+      </ScrollView>
+    );
+  }
+}
